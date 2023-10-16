4db1200006755d52abac12ac8c15d632f11a4571
dezeve <dezeve@outlook.com>
2023-10-16 15:01:56 +0300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,13 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on("ready", () => {
+    console.log("Platform=\t" + process.platform)
+
     console.log("App working");
 
     mainWindow = new BrowserWindow({});
@@ -18,4 +20,45 @@ app.on("ready", () => {
             slashes: true
         })
     )
+
+    const mainMenu = electron.Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+const mainMenuTemplate = [
+    {
+        label: "Exit (CTRL + Q)",
+        accelerator: "Ctrl+Q",
+        role: "quit"
+    },
+    {
+    label: "File",
+    submenu: [
+        {
+            label: "New"
+        },
+        {
+            label: "Open"
+        },
+        {
+            label: "Close"
+        }
+        ]
+    },
+    {
+    label: "Dev Tools",
+    submenu: [
+        {
+            label: "Open Dev Tools",
+            click(item, focusedWindow) {
+                focusedWindow.toggleDevTools();
+            }
+        },
+        {
+            label: "Refresh",
+            accelerator: "Ctrl+R",
+            role: "reload"
+        }
+    ]
+    }
+]
