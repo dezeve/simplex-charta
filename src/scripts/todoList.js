dc82c30d401cca2a0b3a28f2338efebf408ea195
dezeve <dezeve@outlook.com>
2023-11-17 13:48:17 +0300

    Update todo list

--- src/scripts/todoList.js
+++ src/scripts/todoList.js
@@ -4,6 +4,8 @@ const { ipcRenderer } = electron
 const todoData = require("../database/todoData.json")
 const todoItems = todoData.todoItems
 
+const fs = require("fs")
+
 let closeTodoListButton = document.querySelector("#closeTodoListButton")
 let addTodoButton = document.querySelector("#addTodoButton")
 
@@ -19,49 +21,65 @@ addTodoButton.addEventListener("click", () => {
 })
 
 function listTodoItems() {
-    for (const item of todoItems) {
-        const container = document.querySelector(".todo-container")
-    
-        const row = document.createElement("div")
-        row.className = "row card my-2"
-    
-        const cardBody = document.createElement("div")
-        cardBody.className = "card-body text-start row"
-    
-        const p = document.createElement("p")
-        p.className = "card-text col-sm-9"
-        p.innerText = item.text
-    
-        const a = document.createElement("a")
-        a.className = "btn btn-success col-sm-3 align-self-center"
-        a.innerText = "Done"
-    
-        a.addEventListener("click", (e) => {
-            if(confirm("Are you sure to delete this Todo?")){
-                e.target.parentNode.parentNode.remove()
+    for (let index = 0; index < todoItems.length; index++) {
+        const item = todoItems[index];
+
+        const itemIndex = index;
+
+        const container = document.querySelector(".todo-container");
+        const row = document.createElement("div");
+        row.className = "row card my-2";
+
+        const cardBody = document.createElement("div");
+        cardBody.className = "card-body text-start row";
+
+        const p = document.createElement("p");
+        p.className = "card-text col-sm-9";
+        p.innerText = item.text;
+
+        const a = document.createElement("a");
+        a.className = "btn btn-success col-sm-3 align-self-center";
+        a.innerText = "Done";
+
+    a.addEventListener("click", (e) => {
+        if (confirm("Are you sure to delete this Todo?")) {
+            
+            todoItems.splice(itemIndex, 1);
+
+            fs.writeFile(
+            "src/database/todoData.json",
+            JSON.stringify(todoData),
+            (err) => {
+                if (err) {
+                throw err;
+                }
+
+                container.innerHTML = ""
+                listTodoItems()
                 checkTodoCount()
             }
-        })
-    
-        cardBody.appendChild(p)
-        cardBody.appendChild(a)
-    
-        row.appendChild(cardBody)
-    
-        container.appendChild(row)
-        checkTodoCount()
-    }
+        );
+      }
+    });
+
+    cardBody.appendChild(p);
+    cardBody.appendChild(a);
+
+    row.appendChild(cardBody);
+    container.appendChild(row);
+    checkTodoCount();
+  }
 }
 
 function checkTodoCount() {
-    const container = document.querySelector(".todo-container")
-    const alertContainer = document.querySelector(".alert-container")
+    const container = document.querySelector(".todo-container");
+    const alertContainer = document.querySelector(".alert-container");
 
-    document.querySelector(".total-count-container").innerText = container.children.length
+    document.querySelector(".total-count-container").innerText = container.children.length;
 
     if (container.children.length !== 0) {
-        alertContainer.style.display = "none"
+        alertContainer.style.display = "none";
     } else {
-        alertContainer.style.display = "block"
+        alertContainer.style.display = "block";
     }
 }
