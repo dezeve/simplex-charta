a4b8a2d820d59ce928e5c7cb7b989f0bc1cd7b4a
dezeve <dezeve@outlook.com>
2023-11-21 11:18:50 +0300

    Update index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 const electron = require("electron")
 const { ipcRenderer } = electron
 
-const text = document.querySelector("#text")
-
-text.spellcheck = false
+const editor = ace.edit("editor")
+editor.setTheme("ace/theme/dawn")
+editor.session.setMode("ace/mode/javascript")
+editor.setFontSize("17px")
