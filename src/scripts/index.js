af805d710d7b8792a83f0b58da8fb3f19f47dfe2
dezeve <dezeve@outlook.com>
2023-11-21 15:25:39 +0300

    Add live autocompletion

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,14 @@
 const electron = require("electron")
 const { ipcRenderer } = electron
 
+ace.require("ace/ext/language_tools")
+
 const editor = ace.edit("editor")
+
 editor.setTheme("ace/theme/dawn")
 editor.session.setMode("ace/mode/javascript")
 editor.setFontSize("17px")
+
+editor.setOptions({
+    enableLiveAutocompletion: true
+})
