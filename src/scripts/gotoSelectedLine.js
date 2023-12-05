58c92c26bc657ac9d01c56f9578dcce76db9347c
dezeve <dezeve@outlook.com>
2023-12-05 14:42:50 +0300

    Update go to selected line

---
+++ src/scripts/gotoSelectedLine.js
@@ -0,0 +1,5 @@
+const electron = require("electron")
+const { ipcRenderer } = electron
+
+const lineInput = document.querySelector("#lineInput")
+const getSelectedLineInput = document.querySelector("#getSelectedLineInput")
